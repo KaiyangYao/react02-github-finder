dd0c0a57655ad003f02f158433d729c7ff67878b
Kaiyang Yao <yaoky2008@gmail.com>
2022-09-09 03:04:12 -0500

    add user search functionality

--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -9,18 +9,22 @@ const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN;
 export const GithubProvider = ({ children }) => {
   const initialState = {
     users: [],
-    loading: true,
+    loading: false,
   };
 
   const [state, dispatch] = useReducer(githubReducer, initialState);
 
+  const setLoading = () => dispatch({ type: "SET_LOADING" });
+
+  // Get initial users (for testing purpose)
   const fetchUsers = async () => {
+    setLoading();
+
     const response = await fetch(`${GITHUB_URL}/users`, {
       headers: {
         Authorization: `Bearer ${GITHUB_TOKEN}`,
       },
     });
-
     const data = await response.json();
 
     dispatch({
@@ -29,9 +33,44 @@ export const GithubProvider = ({ children }) => {
     });
   };
 
+  // Search users by the username
+  const searchUsers = async (username) => {
+    setLoading();
+
+    const params = new URLSearchParams({
+      q: username,
+    });
+
+    // https://api.github.com/search/users?q=xxx
+    const response = await fetch(`${GITHUB_URL}/search/users?${params}`, {
+      headers: {
+        Authorization: `Bearer ${GITHUB_TOKEN}`,
+      },
+    });
+    const { items } = await response.json();
+
+    dispatch({
+      type: "GET_USERS",
+      payload: items, // can change name to users, but payload is the convention
+    });
+  };
+
+  // Clean up the search result
+  const clearUsers = () => {
+    dispatch({
+      type: "CLEAR_USERS",
+    });
+  };
+
   return (
     <GithubContext.Provider
-      value={{ users: state.users, loading: state.loading, fetchUsers }}
+      value={{
+        users: state.users,
+        loading: state.loading,
+        fetchUsers,
+        searchUsers,
+        clearUsers,
+      }}
     >
       {children}
     </GithubContext.Provider>
--- src/context/github/GithubReducer.js
+++ src/context/github/GithubReducer.js
@@ -1,10 +1,20 @@
 const githubReducer = (state, action) => {
   switch (action.type) {
+    case "SET_LOADING":
+      return {
+        ...state,
+        loading: true,
+      };
     case "GET_USERS":
       return {
         ...state,
         users: action.payload,
         loading: false,
+      };
+    case "CLEAR_USERS":
+      return {
+        ...state,
+        users: [],
       };
     default:
       return state;
